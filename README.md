0fad3ea288a04d1f3a241b61f51e2d968714bb17
Kenji Hikmatullah <iniemailkenji@gmail.com>
2021-07-19 16:42:05 +0700

    add README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+**Docker X Webhook**
+
+Simple practice of using webhook along with docker. Automatically pull and build apps when webhook is called.
+
+Built using
+- React as Frontend Sample
+- Laravel as Backend Sample
+- Nginx
+- Docker
+- Flask as Webhook 
--- webhook/app.py
+++ webhook/app.py
@@ -5,7 +5,7 @@ from subprocess import call
 app = Flask(__name__, template_folder='.')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/webhook', methods=['POST'])
 def index():
   call("./scripts/update-backend.sh", shell=True)
   call('./scripts/update-frontend.sh', shell=True)
